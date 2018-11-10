9699da12607736b58bb4be3eb6471283f8fa74ee
Anto9696 <anto>
2018-11-10 16:39:49 +0100

    Start of project

---
+++ src/main/java/Game.java
@@ -0,0 +1,30 @@
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Game extends BasicGame{
+
+	public Game(String title) {
+		super(title);
+	}
+
+	@Override
+	public void render(GameContainer container, Graphics g) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(GameContainer container) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(GameContainer container, int delta) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/main/java/GameMain.java
+++ src/main/java/GameMain.java
@@ -1,9 +1,24 @@
 import org.junit.*;
 
 import org.newdawn.slick.*;
+import org.lwjgl.LWJGLUtil;
 
 public class GameMain {
+	
+	
+	
     public static void main(String args[]){
-        System.out.println("Hello Maven");
+
+        System.setProperty("org.lwjgl.librarypath",System.getProperty("user.dir")+"/native/"+LWJGLUtil.getPlatformName());
+        System.setProperty("net.java.games.input.librarypath",System.getProperty("user.dir")+"/native/"+LWJGLUtil.getPlatformName());
+       
+        
+		try {
+			 AppGameContainer app = new AppGameContainer(new Game("UNI:ZA"));
+			 app.setDisplayMode(800, 600, false);
+		     app.start();
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
     }
 }
