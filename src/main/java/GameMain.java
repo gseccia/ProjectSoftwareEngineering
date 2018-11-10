3f2650a9a65b1fefba5041292447aeb70f36f523
gseccia <giuseppeseccia1997@gmail.com>
2018-11-10 12:48:12 +0100

    Added Maven to project.

---
+++ src/main/java/GameMain.java
@@ -0,0 +1,11 @@
+package main.java;
+
+import org.junit.*;
+
+import org.newdawn.slick.*;
+
+public class GameMain {
+    public static void main(String args[]){
+        System.out.println("Hello Maven");
+    }
+}
