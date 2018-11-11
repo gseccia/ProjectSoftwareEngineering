4d8e45db6f55060ee5ffebfd285660eebd9c7c48
Anto9696 <anto>
2018-11-11 18:02:11 +0100

    Game starting modified

--- src/main/java/GameMain.java
+++ src/main/java/GameMain.java
@@ -1,11 +1,25 @@
-import org.junit.*;
-
-import org.newdawn.slick.*;
 import org.lwjgl.LWJGLUtil;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
 
-public class GameMain {
+public class GameMain extends StateBasedGame{
+	
+	public final static int MENU = 0;
+	public final static int PLAY = 1;
 	
 	
+	public GameMain(String gamename)
+	{
+		super(gamename);
+	}
+	
+	@Override
+	public void initStatesList(GameContainer container) throws SlickException {
+		this.addState(new Level(PLAY));
+		this.enterState(PLAY); //Enter in MENU in real version
+	}
 	
     public static void main(String args[]){
 
@@ -13,11 +27,13 @@ public class GameMain {
         System.setProperty("net.java.games.input.librarypath",System.getProperty("user.dir")+"/native/"+LWJGLUtil.getPlatformName());
        
 		try {
-			 AppGameContainer app = new AppGameContainer(new Game("UNI:ZA"));
+			 AppGameContainer app = new AppGameContainer(new GameMain("UNI:ZA"));
 			 app.setDisplayMode(800, 600, false);
 		     app.start();
 		} catch (SlickException e) {
 			e.printStackTrace();
 		}
     }
+
+
 }
