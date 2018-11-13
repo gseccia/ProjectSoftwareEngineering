56cadb1aac664b374f0177896dccaa7eccb5fb56
bkoala42 <ilariagigi@virgilio.it>
2018-11-13 17:11:08 +0100

    added maps

---
+++ resource/maps/CompleteSquare/buddhas.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="buddhas" tilewidth="16" tileheight="16" tilecount="66" columns="11">
+ <image source="buddhas.png" width="176" height="96"/>
+</tileset>
