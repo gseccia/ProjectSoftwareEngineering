13a7d2e90f9bdfec9d0a71214f869f7931013da2
Anto9696 <anto>
2018-11-14 15:41:47 +0100

    tmp

---
+++ src/elements/MultiAnimatable.java
@@ -0,0 +1,30 @@
+package elements;
+
+public interface MultiAnimatable {
+
+    /**
+     * Shows the up animation of the character
+     */
+    public void faceUp();
+
+    /**
+     * Shows the down animation of the character
+     */
+    public void faceDown();
+
+    /**
+     * Shows the left animation of the character
+     */
+    public void faceLeft();
+
+    /**
+     * Shows the right animation of the character
+     */
+    public void faceRight();
+
+    /**
+     * Shows the still animation of the character
+     */
+    public void faceStill();
+
+}
