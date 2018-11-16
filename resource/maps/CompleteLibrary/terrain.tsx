2d98c433f8f59bae76ccfc0fb55d60bed8b0d098
bkoala42 <ilariagigi@virgilio.it>
2018-11-16 11:15:36 +0100

    tested and fixed maps

--- resource/maps/CompleteLibrary/terrain.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.0" name="terrain" tilewidth="16" tileheight="16" tilecount="1932" columns="42">
- <image source="terrain.png" width="672" height="736"/>
-</tileset>
