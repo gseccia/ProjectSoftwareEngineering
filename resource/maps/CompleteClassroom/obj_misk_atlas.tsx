ab1772ca0347738562f031dacea18f7c784ad57f
bkoala42 <ilariagigi@virgilio.it>
2018-11-17 17:57:02 +0100

    added new Classroom map

--- resource/maps/CompleteClassroom/obj_misk_atlas.tsx
+++ resource/maps/CompleteClassroom/obj_misk_atlas.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.0" name="obj_misk_atlas" tilewidth="16" tileheight="16" tilecount="4096" columns="64">
- <image source="../Biblioteca/Atlas2/obj_misk_atlas.png" width="1024" height="1024"/>
+ <image source="obj_misk_atlas.png" width="1024" height="1024"/>
 </tileset>
