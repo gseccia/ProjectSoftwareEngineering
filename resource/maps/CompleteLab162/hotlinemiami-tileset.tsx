68d926838e6c7ef48960d6695548510508e6e163
bkoala42 <ilariagigi@virgilio.it>
2018-11-18 23:58:00 +0100

    new map

---
+++ resource/maps/CompleteLab162/hotlinemiami-tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="hotlinemiami-tileset" tilewidth="16" tileheight="16" tilecount="2028" columns="52">
+ <image source="hotlinemiami-tileset.png" width="844" height="627"/>
+</tileset>
