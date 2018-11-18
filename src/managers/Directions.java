a13a6e2ddee90e1be06f545da8e0bee543e1dbb6
TestaDiRapa <v.c.pierro@gmail.com>
2018-11-18 13:00:48 +0100

    Added interface to collision manager

---
+++ src/managers/Directions.java
@@ -0,0 +1,12 @@
+package managers;
+
+import org.newdawn.slick.Input;
+
+public class Directions {
+
+    public static final int UP = Input.KEY_W;
+    public static final int DOWN = Input.KEY_S;
+    public static final int LEFT = Input.KEY_A;
+    public static final int RIGHT = Input.KEY_D;
+
+}
--- src/managers/MapCollisionManager.java
+++ src/managers/MapCollisionManager.java
@@ -1,17 +1,15 @@
 package managers;
 
-import  org.newdawn.slick.Input;
 import org.newdawn.slick.tiled.TiledMap;
 
 import elements.Mob;
-import elements.NullAnimationException;
 
 /**
  * 
  * @author JBFourierous
  * This class manages collisions of the character with map elements.
  */
-public class MapCollisionManager {
+public class MapCollisionManager implements MapCollisionInterface {
 	//int shiftX, shiftY;
 	//private String mapName;
 	private TiledMap map;
@@ -38,40 +36,57 @@ public class MapCollisionManager {
 		}
 	}
 
-
-	public boolean wallCollision(int shiftX, int shiftY, Mob player, int key) throws NullAnimationException {
+	/**
+	 * Checks if a mob collides with a wall
+	 *
+	 * @param shiftX the x shift of the map
+	 * @param shiftY the y shift of the map
+	 * @param player the mob that collides
+	 * @param key    the direction of the movement
+	 * @return true if collide, false otherwise
+	 */
+	@Override
+	public boolean wallCollision(int shiftX, int shiftY, Mob player, int key) {
 		// TODO Auto-generated method stub
 		//Input in = gc.getInput();
 		//System.out.println(player.getX());
 		//System.out.println(map);
 
 		//System.out.println(((int)player.getX()/map.getTileWidth())+" ,"+(int)player.getY()/map.getTileHeight());
-		
+
 		// DA DOVE PRENDERE shift_x e shift_y?
 		int pXPosition = (int)(player.getX()/map.getTileWidth()) + shiftX;
 		int pYPosition = (int)(player.getY()/map.getTileHeight()) + shiftY;
 		//System.out.println(pXPosition+","+pYPosition);
-		
-		if (key == Input.KEY_D) {
+
+		if (key == Directions.RIGHT) {
 			//Blocked tile
 			return (!collisionArray[pXPosition + 1][pYPosition]);
 		}
-		else if (key == Input.KEY_A) {
+		else if (key == Directions.LEFT) {
 			return (!collisionArray[pXPosition - 1][pYPosition]);
 		}
-		else if (key == Input.KEY_S) {
+		else if (key == Directions.DOWN) {
 			return (!collisionArray[pXPosition][pYPosition + 1]);
 		}
-		else if (key == Input.KEY_W) {
+		else if (key == Directions.UP) {
 			return (!collisionArray[pXPosition][pYPosition - 1]);
 		}
 		else
 			return false;
 	}
 
+	/**
+	 * Checks if a player collides with a door
+	 *
+	 * @param shiftX the x shift of the map
+	 * @param shiftY the y shift of the map
+	 * @param player the player object that collides
+	 * @return true if collide, false otherwise
+	 */
+	@Override
 	public boolean doorCollision(int shiftX, int shiftY, Mob player) {
 		//TO BE IMPLEMENTED
 		return false;
 	}
-
 }
