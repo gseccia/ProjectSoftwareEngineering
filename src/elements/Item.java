8001045d262b2ce17c5186fbd687ce0538851a6b
JBFourierous <marco.carpentiero@gmail.com>
2018-11-25 14:59:03 +0100

    Small update for intersection

--- src/elements/Item.java
+++ src/elements/Item.java
@@ -2,6 +2,7 @@ package elements;
 
 import missions.MissionItem;
 import org.newdawn.slick.Animation;
+import org.newdawn.slick.geom.Rectangle;
 
 public class Item extends AnimatedElement implements MissionItem {
 
@@ -11,6 +12,32 @@ public class Item extends AnimatedElement implements MissionItem {
         //TODO change with values from configuration
         super(new Animation(), 0, 0, 0, 0);
         this.id = id;
+    }
+    
+    /**
+     * Overrides (?) the original intersects method to avoid annoying collisions due to rubbing
+     * @param r colliding element
+     * @return true if intersection is verified, otherwise false
+     */
+    public boolean intersects(Rectangle r){
+        if(super.intersects(r)){
+            if(r.getY() == (y+height)){
+                return false;
+            }
+            else if((r.getY()+r.getHeight()) == y){
+                return false;
+            }
+            else if(r.getX() == (x+width)){
+                return false;
+            }
+            else if((r.getX()+r.getWidth()) == x){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
