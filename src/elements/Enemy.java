dfcb1fa02a0851bbb81c9ef81db833821dade1e5
Anto9696 <anto>
2018-11-25 16:55:38 +0100

    Enemyù movements!

--- src/elements/Enemy.java
+++ src/elements/Enemy.java
@@ -5,6 +5,9 @@ import configuration.NoSuchElementInConfigurationException;
 import main.java.Block;
 import managers.Directions;
 import missions.MissionItem;
+
+import java.util.Random;
+
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Rectangle;
 
@@ -15,6 +18,8 @@ public class Enemy extends Mob implements MissionItem,Runnable {
     private Block map;
     private int direction;
     private Rectangle vision;
+    private int directVision = 10;
+    private int lateralVision = 1;
     private Player player;
     
     public Enemy(MobConfiguration configuration, String id, Block map, Player p) throws NoSuchElementInConfigurationException, SlickException, NullAnimationException {
@@ -22,9 +27,7 @@ public class Enemy extends Mob implements MissionItem,Runnable {
         this.id = id;
         this.map = map;
         this.player = p;
-        vision = new Rectangle(0,0,3,1);  // Vision 
         direction = Directions.LEFT;     // Suppose initial direction right
-        
     }
 
     @Override
@@ -34,7 +37,9 @@ public class Enemy extends Mob implements MissionItem,Runnable {
     
     public void init(int x,int y) {
     	setLocation(x+map.getShiftX()*map.getMap().getTileWidth(),y-map.getShiftY()*map.getMap().getTileHeight());
-    	vision.setLocation(getX(), getY());
+    	directVision *= map.getMap().getTileWidth();
+    	lateralVision *= map.getMap().getTileHeight();
+    	vision = new Rectangle(getX(), getY(),directVision,lateralVision);  // Vision 
     }
     
     public void draw() {
@@ -52,48 +57,90 @@ public class Enemy extends Mob implements MissionItem,Runnable {
     
     public void update() throws NullAnimationException {
     	float x,y;
+    	boolean attack;
 		 // Until I'm alive
 		if(getHp()>0) {
 			x = getX();
 			y = getY();
 			
-			if (direction == Directions.RIGHT) vision.setLocation(getX()-map.getShiftX()*map.getMap().getTileWidth()-vision.getWidth(), getY()-map.getShiftY()*map.getMap().getTileHeight());
-			else vision.setLocation(getX()-map.getShiftX()*map.getMap().getTileWidth(), getY()-map.getShiftY()*map.getMap().getTileHeight());
+			
+			setLocation((int)(getX())-map.getShiftX()*map.getMap().getTileWidth(),(int)(getY())-map.getShiftY()*map.getMap().getTileHeight());
+			
+			switch(direction) {
+				case Directions.LEFT:
+					vision.setBounds(getX()-directVision, getY(),directVision,lateralVision);
+					break;
+				case Directions.RIGHT:
+					vision.setBounds(getX(), getY(),directVision,lateralVision);
+					break;
+				case Directions.UP:
+					vision.setBounds(getX(), getY()-directVision,lateralVision,directVision);
+					break;
+				case Directions.DOWN:
+					vision.setBounds(getX(), getY(),lateralVision,directVision);
+					break;
+			}
+			
 			if(vision.intersects(player)) {
 				// Attack him!
+				attack = true;
 				System.out.println(id+" "+map.getID()+" ATTACK!");
 			}
-			else {
-				
-				setLocation((int)(getX())-map.getShiftX()*map.getMap().getTileWidth(),(int)(getY())-map.getShiftY()*map.getMap().getTileHeight());
-				
-				if(!map.getCollisionManager().wallCollision(map.getShiftX(), map.getShiftY(), this, direction)) {
+			else attack = false;
+			
+			
+			if(!map.getCollisionManager().wallCollision(map.getShiftX(), map.getShiftY(), this, direction)) {
 					System.out.println(id+" "+map.getID()+" COLLIDE");
-					if (direction == Directions.RIGHT) {
-						direction = Directions.LEFT;
+					
+					//  Choose a random free direction
+					int choosen = direction;
+					Random r = new Random();
+					while(choosen == direction) {
+						switch(r.nextInt(4)) {
+							case 0:
+								choosen = Directions.UP;
+								break;
+							case 1:
+								choosen = Directions.RIGHT;
+								break;
+							case 2:
+								choosen = Directions.DOWN;
+								break;
+							case 3:
+								choosen = Directions.LEFT;
+								break;
+						}
 					}
-					else {
-						direction = Directions.RIGHT;
-					}
+					
+					direction = choosen;	
 				}
 				
 				setLocation(x,y);
 				switch(direction) {
 					case Directions.LEFT:
-						x = x - 1;//map.getMap().getTileWidth();
+						x = x - 1;
 						System.out.println(id+" "+map.getID()+" LEFT");
 						faceLeft();
 						break;
 					case Directions.RIGHT:
-						x = x + 1;//map.getMap().getTileHeight();
+						x = x + 1;
 						System.out.println(id+" "+map.getID()+" RIGHT");
 						faceRight();
 						break;
+					case Directions.UP:
+						y = y - 1;
+						System.out.println(id+" "+map.getID()+" UP");
+						faceUp();
+						break;
+					case Directions.DOWN:
+						y = y + 1;
+						System.out.println(id+" "+map.getID()+" DOWN");
+						faceDown();
+						break;
 				}
-				setLocation(x,y);
-			}
+			setLocation(x,y);
 		}
-    }
+	}
 
     
 	@Override
