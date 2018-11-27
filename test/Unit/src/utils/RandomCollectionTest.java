0c1cc99dcd55ccefa99bfbca4a666b86e09ffa73
bkoala42 <ilariagigi@virgilio.it>
2018-11-27 17:31:09 +0100

    random shit fixed

--- test/Unit/src/utils/RandomCollectionTest.java
+++ test/Unit/src/utils/RandomCollectionTest.java
@@ -2,20 +2,20 @@ package Unit.src.utils;
 
 import org.junit.Before;
 import org.junit.Test;
-import utils.RandomHashSet;
+import utils.RandomCollection;
 
 import java.util.Iterator;
 
 import static org.junit.Assert.*;
 
-public class RandomHashSetTest {
+public class RandomCollectionTest {
 
     private long seed = 123456789;
-    private RandomHashSet<Integer> rhs;
+    private RandomCollection<Integer> rhs;
 
     @Before
     public void setUp(){
-        rhs = new RandomHashSet<>(seed);
+        rhs = new RandomCollection<>(seed);
         for(int i=0; i<5; i++){
             rhs.add(i);
         }
