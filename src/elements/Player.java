df01039aa96c91aa50cb07e273d2aa61cb332a91
TestaDiRapa <v.c.pierro@gmail.com>
2018-11-29 18:37:57 +0100

    Handles the attack direction in a simpler way

--- src/elements/Player.java
+++ src/elements/Player.java
@@ -1,5 +1,6 @@
 package elements;
 
+import attacks.Attack;
 import attacks.StandardPlayerAttack;
 import configuration.MobConfiguration;
 import configuration.NoSuchElementInConfigurationException;
@@ -11,4 +12,12 @@ public class Player extends Mob {
         super(configuration, id);
         setAttack(new StandardPlayerAttack(this));
     }
+
+    @Override
+    public Attack getAttack(){
+        Attack tmp = super.getAttack();
+        tmp.setHitbox();
+        return tmp;
+    }
+
 }
