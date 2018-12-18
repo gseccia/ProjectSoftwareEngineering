65682ac6d2df36425639274595f22e0bd1cac72c
TestaDiRapa <v.c.pierro@gmail.com>
2018-12-18 20:32:47 +0100

    Updated mission tests

--- test/Unit/src/configuration/SpecialAttackConfigurationTest.java
+++ test/Unit/src/configuration/SpecialAttackConfigurationTest.java
@@ -6,15 +6,23 @@ import org.junit.Before;
 import org.junit.Test;
 import org.newdawn.slick.SlickException;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.assertEquals;
 
 public class SpecialAttackConfigurationTest {
 
     private SpecialAttackConfiguration s;
+    private Set<String> names;
 
     @Before
     public void setUp(){
         s = SpecialAttackConfiguration.getInstance();
+        names = new HashSet<>();
+        names.add("horahora");
+        names.add("sparagmos");
+        names.add("iuf");
     }
 
     @Test
@@ -31,6 +39,21 @@ public class SpecialAttackConfigurationTest {
     @Test(expected = NoSuchElementInConfigurationException.class)
     public void testGetIconThrowsExceptionIfIDIsNotValid() throws SlickException, NoSuchElementInConfigurationException {
         s.getIcon("no");
+    }
+
+    @Test(expected = NoSuchElementInConfigurationException.class)
+    public void testGetNameThrowsExceptionIfIDIsNotValid() throws NoSuchElementInConfigurationException {
+        s.getSpecialAttackName("no");
+    }
+
+    @Test(expected = NoSuchElementInConfigurationException.class)
+    public void testGetDescriptionThrowsExceptionIfIDIsNotValid() throws NoSuchElementInConfigurationException {
+        s.getSpecialAttackDescription("no");
+    }
+
+    @Test
+    public void testAttackNamesAreReadCorrectly() {
+        assertEquals(names, s.getSpecialAttackNames());
     }
 
 
