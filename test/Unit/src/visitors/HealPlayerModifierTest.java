5fbf20a2957812cc37391348ed2da77c53dd6a2f
TestaDiRapa <v.c.pierro@gmail.com>
2018-12-19 16:28:57 +0100

    Added visitors tests

---
+++ test/Unit/src/visitors/HealPlayerModifierTest.java
@@ -0,0 +1,46 @@
+package Unit.src.visitors;
+
+import elements.Player;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import visitors.HealPlayerModifier;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.doAnswer;
+
+public class HealPlayerModifierTest {
+
+    private final int DAMAGE = 20;
+    private final int LIFE = 100;
+
+    @Mock
+    private Player mockPlayer;
+
+    private HealPlayerModifier visitor;
+    private int life = LIFE;
+
+    @Before
+    public void setUp() {
+        visitor = new HealPlayerModifier(DAMAGE);
+
+        mockPlayer = Mockito.mock(Player.class);
+        doAnswer((i) -> {
+            life -= (int)i.getArgument(0);
+            return null;
+        }).when(mockPlayer).damage(anyInt());
+    }
+
+    @Test
+    public void testHealingIsCalculatedCorrectly(){
+        visitor.accept(mockPlayer);
+        assertEquals(LIFE+DAMAGE, life);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testVisitorThrowsExceptionIfPlayerIsNull(){
+        visitor.accept(null);
+    }
+}
