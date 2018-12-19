e29763fdba520fc05862e0cc01707d14f153ce9a
Army96 <armandocavallaro@outlook.com>
2018-12-19 22:35:06 +0100

    .

--- src/missions/FindNTrapsMission.java
+++ src/missions/FindNTrapsMission.java
@@ -93,7 +93,7 @@ public class FindNTrapsMission extends Mission {
 
     @Override
     public String toString(){
-        String ret = "You have to find " + numTraps + " traps [" + (numTraps-targets.size()) + "/" + numTraps +"]";
+        String ret = "Find " + numTraps + " traps [" + (numTraps-targets.size()) + "/" + numTraps +"]";
         if(completed()){
             return ret + "\n- COMPLETED!";
         }
--- src/missions/KillTheBossMission.java
+++ src/missions/KillTheBossMission.java
@@ -85,7 +85,7 @@ public class KillTheBossMission extends Mission {
 
     @Override
     public String toString(){
-        String ret = "You have to kill the " + getTargetId() + " boss";
+        String ret = "Kill the " + getTargetId() + " boss";
         if(completed()){
             return ret + "\n- COMPLETED!";
         }
