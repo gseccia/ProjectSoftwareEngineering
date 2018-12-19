25d09a7719b52c8355f39e5bc91e454398e7b4ca
TestaDiRapa <v.c.pierro@gmail.com>
2018-12-19 18:32:22 +0100

    Added javadoc for the configuration package

--- src/configuration/EnemyConfiguration.java
+++ src/configuration/EnemyConfiguration.java
@@ -2,11 +2,18 @@ package configuration;
 
 import java.util.Set;
 
+/**
+ * Configuration class for the enemies.
+ * Implemented as Singleton
+ */
 public class EnemyConfiguration extends MobConfiguration {
 
     private static final String defaultFilename = System.getProperty("user.dir") + "/resource/configurations/enemy.conf";
     private static EnemyConfiguration instance;
 
+    /**
+     * @return the instance of the singleton
+     */
     public static EnemyConfiguration getInstance() {
         if(instance == null){
             instance = new EnemyConfiguration(defaultFilename);
@@ -14,10 +21,19 @@ public class EnemyConfiguration extends MobConfiguration {
         return instance;
     }
 
+    /**
+     * Constructor
+     * @param filename the filename of the configuration file
+     */
     private EnemyConfiguration(String filename){
         super(filename);
     }
 
+    /**
+     * @param id the mob id
+     * @return the mob points
+     * @throws NoSuchElementInConfigurationException if there isn't this id in the configuration
+     */
     public int getMobPoints(String id) throws NoSuchElementInConfigurationException {
         if(getConfiguration(id).get("points") == null) {
             throw new NoSuchElementInConfigurationException();
@@ -25,10 +41,18 @@ public class EnemyConfiguration extends MobConfiguration {
         return this.getConfiguration(id).get("points").getAsInt();
     }
 
+    /**
+     * @param id the mob id
+     * @return the time to wait between two attack in frames
+     * @throws NoSuchElementInConfigurationException if there isn't this id in the configuration
+     */
     public int getAttackLatency(String id) throws NoSuchElementInConfigurationException {
         return getConfiguration(id).get("delay").getAsInt();
     }
 
+    /**
+     * @return a Set containing all the mobs' ids
+     */
     public Set<String> getMobNames(){
         return getIdSet();
     }
--- src/configuration/SoundConfiguration.java
+++ src/configuration/SoundConfiguration.java
@@ -4,12 +4,20 @@ import com.google.gson.JsonObject;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.Sound;
 
+/**
+ * Configuration class for the sounds.
+ * Implemented as Singleton
+ */
 public class SoundConfiguration extends Configuration {
 
     private static final String filename = System.getProperty("user.dir") + "/resource/configurations/sound.conf";
     private static SoundConfiguration instance;
     private JsonObject configuration;
 
+    /**
+     * Constructor
+     * @param filename the filename of the configuration file
+     */
     private SoundConfiguration(String filename) {
         this.configuration = super.uploadConfiguration(filename);
     }
@@ -22,6 +30,9 @@ public class SoundConfiguration extends Configuration {
         return this.configuration.getAsJsonObject(id);
     }
 
+    /**
+     * @return the instance of the SoundConfiguration
+     */
     public static SoundConfiguration getInstance() {
         if(instance == null){
             instance = new SoundConfiguration(filename);
@@ -29,11 +40,23 @@ public class SoundConfiguration extends Configuration {
         return instance;
     }
 
+    /**
+     * @param id the id of an item/mob
+     * @return the healing sound
+     * @throws NoSuchElementInConfigurationException if there is no such id in the configuration
+     * @throws SlickException a slick exception
+     */
     public Sound getHealSound(String id) throws NoSuchElementInConfigurationException, SlickException {
         String path = getConfiguration(id).get("heal").getAsString();
         return new Sound(System.getProperty("user.dir")+"/"+path);
     }
 
+    /**
+     * @param id the id of an item/mob
+     * @return the power up sound
+     * @throws NoSuchElementInConfigurationException if there is no such id in the configuration
+     * @throws SlickException a slick exception
+     */
     public Sound getPowerUpSound(String id) throws NoSuchElementInConfigurationException, SlickException {
         String path = getConfiguration(id).get("power").getAsString();
         return new Sound(System.getProperty("user.dir")+"/"+path);
