450a088f8cb7cd0d5394da6b8034f4aa41494442
bkoala42 <ilariagigi@virgilio.it>
2018-12-21 12:43:27 +0100

    updated test

--- src/main/GameMain.java
+++ src/main/GameMain.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.io.File;
+
 import org.lwjgl.LWJGLUtil;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.SlickException;
@@ -10,9 +12,18 @@ public class GameMain{
 
     public static void main(String[] args){
 
-        System.setProperty("org.lwjgl.librarypath",System.getProperty("user.dir")+"/lib/native/"+LWJGLUtil.getPlatformName());
-        System.setProperty("net.java.games.input.librarypath",System.getProperty("user.dir")+"/lib/native/"+LWJGLUtil.getPlatformName());
+        System.setProperty("org.lwjgl.librarypath",
+        		System.getProperty("user.dir")+File.separator+
+        		"lib"+File.separator+"native"+File.separator
+        		+LWJGLUtil.getPlatformName());
         
+        System.setProperty("net.java.games.input.librarypath",
+        		System.getProperty("user.dir")+File.separator+
+        		"lib"+File.separator+"native"+File.separator+LWJGLUtil.getPlatformName());
+//        System.setProperty("java.library.path",
+//        		System.getProperty("user.dir")+
+//        		"\\Lib\\native\\"+LWJGLUtil.getPlatformName());
+        System.out.println(System.getProperty("org.lwjgl.librarypath"));
         
 		try {
 			AppGameContainer app = new AppGameContainer(new Game("UNI:ZA"));
--- test/Unit/src/managers/CollisionDetectionPlayerAttacksEnemyTest.java
+++ test/Unit/src/managers/CollisionDetectionPlayerAttacksEnemyTest.java
@@ -15,14 +15,13 @@ import org.newdawn.slick.tiled.TiledMap;
 
 import attacks.Attack;
 import elements.Mob;
-import managers.CollisionDetectionEnemyAttacksPlayer;
 import managers.CollisionDetectionPlayerAttacksEnemy;
 import managers.HitboxMaker;
 
 public class CollisionDetectionPlayerAttacksEnemyTest {
 	
 	@Mock private HitboxMaker hitbox;
-	@Mock private Mob player, mob, tmp;
+	@Mock private Mob player, armandomorto;
 	@Mock private Attack att;
 	@Mock private CollisionDetectionPlayerAttacksEnemy cdpae;
 	
@@ -30,33 +29,30 @@ public class CollisionDetectionPlayerAttacksEnemyTest {
 	public void setUp() {
 		this.hitbox = Mockito.mock(HitboxMaker.class);
 		this.player = Mockito.mock(Mob.class);
-		this.mob =  Mockito.mock(Mob.class);
-		this.tmp = Mockito.mock(Mob.class);
+		this.armandomorto = Mockito.mock(Mob.class);
 		this.att = Mockito.mock(Attack.class);
-		this.cdpae = Mockito.mock(CollisionDetectionPlayerAttacksEnemy.class);
-		
-		Mockito.when(this.hitbox.getMap()).thenReturn(Mockito.mock(TiledMap.class));
-		List<Mob> mobs = new ArrayList<>();
-		mobs.add(tmp);
-		Mockito.when(this.hitbox.getMobs()).thenReturn(mobs);
-		
-		List<Mob> enemy = new LinkedList<>();
-		Mockito.when(this.hitbox.getMobs().get(0).getHp() <= 0).thenReturn(true);
-		Mockito.when(this.hitbox.getMobs().get(0).getHp() > 0).thenReturn(false);
-		mobs.remove(0);
-		
-		this.hitbox.getMobs().add(player);
-		this.hitbox.getMobs().add(mob);
-		Mockito.when(this.tmp.getAttack().intersects(this.hitbox.getMobs().get(0))).thenReturn(true);
-		Mockito.when(this.tmp.getAttack().intersects(this.hitbox.getMobs().get(1))).thenReturn(false);
-		Mockito.when(this.tmp.isReadyToAttack()).thenReturn(true);
-		Mockito.when(this.tmp.isReadyToAttack()).thenReturn(true);
-		enemy.add(mobs.get(0));
+
+		Mockito.when(this.player.isReadyToAttack()).thenReturn(false);
 		
 		Mockito.when(this.player.getX()).thenReturn(0f);
 		Mockito.when(this.player.getY()).thenReturn(0f);
-		Mockito.when(this.mob.getX()).thenReturn(0f);
-		Mockito.when(this.mob.getY()).thenReturn(0f);
+		Mockito.when(this.armandomorto.getX()).thenReturn(0f);
+		Mockito.when(this.armandomorto.getY()).thenReturn(0f);
+		
+//		Mockito.when(this.armandomorto.getHp()).thenReturn(0);
+		
+		Mockito.when(this.player.getAttack()).thenReturn(att);
+		Mockito.when(this.player.getAttack().intersects(armandomorto)).thenReturn(true);
+		
+//		mock hitbox
+		Mockito.when(this.hitbox.getMap()).thenReturn(Mockito.mock(TiledMap.class));
+		List<Mob> l = new ArrayList<>();
+		l.add(armandomorto);
+		Mockito.when(this.hitbox.getMobs()).thenReturn(l);
+		this.cdpae = Mockito.mock(CollisionDetectionPlayerAttacksEnemy.class);
+//		mock CollisionDetectionPlayerAttacksEnemy
+		Mockito.when(this.cdpae.getMobs()).thenReturn(l);
+//		Mockito.when(this.cdpae.getMobs().get(0).getHp()).thenReturn(0);
 	}
 	
 	@Test
@@ -65,20 +61,17 @@ public class CollisionDetectionPlayerAttacksEnemyTest {
 	}
 	@Test
 	public void getEnemyTest() {
-		CollisionDetectionPlayerAttacksEnemy cdpae = Mockito.mock(CollisionDetectionPlayerAttacksEnemy.class);
-		List<Mob> tmp = new LinkedList<>();
-		Mockito.when(cdpae.getEnemy()).thenReturn(tmp);
-		assertEquals(1, cdpae.getEnemy());
+		assertEquals(1, cdpae.getEnemy().size());
 	}
-	
 	@Test
 	public void detectCollisionTestTrue() {
-		assertEquals(false, cdpae.detectCollision(0, 0, player));
+		Mockito.when(this.hitbox.getMobs().get(0).getHp()).thenReturn(50);
+		assertEquals(true, cdpae.detectCollision(0, 0, player));
 	}
 	
 	@Test
 	public void detectCollisionTestFalse() {
-		CollisionDetectionEnemyAttacksPlayer cdd = new CollisionDetectionEnemyAttacksPlayer(this.hitbox);
-		assertEquals(false, cdpae.detectCollision(0, 0, mob));
+		Mockito.when(this.hitbox.getMobs().get(0).getHp()).thenReturn(0);
+		assertEquals(false, cdpae.detectCollision(0, 0, player));
 	}
 }
